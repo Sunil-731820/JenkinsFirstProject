c25b0fbab9180ba3e86b60cf315c9d05e7014227
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2025-01-12 20:30:16 +0530

    Succes parts

--- src/main/java/com/java/mvn/Hello.java
+++ src/main/java/com/java/mvn/Hello.java
@@ -1,7 +1,7 @@
 package com.java.mvn;
 
 public class Hello {
-	public static void (String[] args) {
+	public static void main(String[] args) {
 		System.out.println("This is the first prject Build using jenkins Tools ");
 	}
 
